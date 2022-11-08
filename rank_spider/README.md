966947e2a8e577424ca2b5a09d3cb2a04642a1a6
mushan <mushan.zb@gmail.com>
2022-11-08 22:10:40 +0800

    feat: 新增榜单爬虫

---
+++ rank_spider/README.md
@@ -0,0 +1,35 @@
+# Python 爬虫爬取榜单说明
+
+## 样例说明
+
+## 使用说明
+
+## 爬虫入门
+> 使用 Python 完成爬虫功能，至少需要了解 requests、BeautifulSoup、selenium 三个库
+
+
+### requests
+> requests 
+
+[官方库](https://pypi.org/project/requests/)
+[接口文档](https://requests.readthedocs.io/en/latest/)
+
+
+### BeautifulSoup
+> BeautifulSoup 提供一些简单的、python 式的函数用来处理导航、搜索、修改分析树等功能。通常又称为：bs4
+
+[官方库](https://pypi.org/project/beautifulsoup4/)
+[接口文档](https://www.crummy.com/software/BeautifulSoup/bs4/doc.zh/)
+
+
+### selenium
+> Selenium 是一个将浏览器自动化的工具，能模拟用户在浏览器中行为的工具
+[官方库](https://pypi.org/project/selenium/)
+[接口文档](https://www.selenium.dev/selenium/docs/api/py/api.html)
+[中文文档](https://python-selenium-zh.readthedocs.io/zh_CN/latest/)
+[Chrome 驱动器](https://chromedriver.chromium.org/downloads)
+**注：**建议安装和自己电脑版本一致的驱动器
+
+
+
+
---
+++ rank_spider/ccpcoj.py
@@ -0,0 +1,142 @@
+
+import time
+import re
+import requests
+import rank
+
+from typing import Any, Dict, List, Tuple
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+
+class CCPCOJ:
+    '''当页面很复杂，而且自己不想去排查页面的各种异步请求时，可通过 selenium 将整个页面加载之后获取网页的 html 代码进行解析'''
+    def __init__(self) -> None:
+        option = Options()
+        option.add_argument('--headless')
+        option.add_argument("--disable-blink-features=AutomationControlled")
+        self.driver = webdriver.Chrome('/etc/chromedriver', options=option)
+
+    def __del__(self) -> None:
+        self.driver.close()
+
+    def spider(self, url: str) -> str:
+        self.driver.get(url=url)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        
+        return self.driver.page_source
+
+
+def get_page(url: str) -> str:
+    try:
+        result = requests.get(url=url, timeout=5)
+    except Exception as e:
+        print('请求 URL 发生错误', e)
+        return
+
+    if result.status_code != 200:
+        print("请求被拒绝，状态码：", result.status_code)
+        return
+
+    result.encoding = 'utf-8'
+    return result.text
+
+
+class Parse:
+    def __init__(self, text) -> None:
+        soup = BeautifulSoup(text, 'lxml')
+        self.table = soup.find('table', id='rank')
+
+    def contest(self) -> rank.Contest:
+        return rank.Contest('第五届中国大学生程序设计竞赛总决赛---中国传媒大学', 1573952400, 5, 1)
+
+    def problems(self) -> List[rank.Problem]:
+        return [rank.Problem('A'), rank.Problem('B'), rank.Problem('C'), rank.Problem('D'), rank.Problem('E'), 
+            rank.Problem('F'), rank.Problem('G'), rank.Problem('H'), rank.Problem('I'), rank.Problem('J'), 
+            rank.Problem('K'), rank.Problem('L')]
+
+    def series(self) -> List[rank.Series]:
+        self.gold = 13
+        self.silver = 36
+        self.bronze = 69
+        return [rank.Series('#', [('一等奖', self.gold, rank.Style_Gold), ('二等奖', self.silver, rank.Style_Silver), ('三等奖', self.bronze, rank.Style_Bronze)]), rank.Series('S#')]
+
+    def rows(self) -> List[rank.Row]:
+        rows = []
+
+        tbody = self.table.find('tbody')
+        for i, tr in enumerate(tbody.find_all('tr')):
+            tds = tr.find_all('td')
+
+            official = True
+            if tds[0].string == '*':
+                official = False
+            user = rank.User(name=tds[3].string, organization=tds[4].string, official=official)
+
+            rk = rank.Order()
+            if tds[0].string == 'Winner':
+                rk = rank.Order(1, 0)
+            elif tds[0].string != '*':
+                index = int(tds[0].string)
+                if index < self.gold:
+                    rk = rank.Order(index, 0)
+                elif index < self.silver:
+                    rk = rank.Order(index, 1)
+                elif index < self.bronze:
+                    rk = rank.Order(index, 2)
+                else:
+                    rk = rank.Order(index)
+
+            srk = rank.Order()
+            if tds[1].string is not None and tds[0].string != '*':
+                srk = rank.Order(int(tds[1].string))
+            orders = [rk, srk]
+
+            h, m, s = tds[6].string.split(':')
+            time_s = int(h)*60*60 + int(m)*60 + int(s)
+            score = (int(tds[5].string), time_s)
+
+            status = []
+            for i in range(0, 12):
+                ps = tds[i+7].string
+                # print(f'ps={ps}')
+                if ps is None:
+                    status.append(rank.Status())
+                    continue
+                
+                tries_s = re.compile(r'-[0-9]+').findall(ps)
+                # print(f'tries_s={tries_s}')
+                tries = 1
+                if len(tries_s) > 0:
+                    tries = -1 * int(tries_s[0]) + 1
+                
+                tm_s = re.compile(r'[0-9]+:[0-9]+:[0-9]+').findall(ps)
+                # print(f'tm_s={tm_s}')
+                if len(tm_s) > 0:
+                    h, m, s = tm_s[0].split(':')
+                    tm = int(h)*60*60 + int(m)*60 + int(s)
+                    status.append(rank.Status(rank.SR_Accepted, tm, tries))
+                    continue
+                status.append(rank.Status(rank.SR_Rejected, 0, tries))
+                
+            rows.append(rank.Row(orders, user, score, status))
+        
+        return rows
+
+
+def main():
+    # page = Scoreboard().spider('http://acm.sdut.edu.cn/acmss/ccpc/2019/beijing/index.html')
+    page = get_page('http://acm.sdut.edu.cn/acmss/ccpc/2019/beijing/index.html')
+    parse = Parse(page)
+    contest = parse.contest()
+    problems = parse.problems()
+    series = parse.series()
+    rows = parse.rows()
+    r = rank.Rank(contest, problems, series, rows)
+    print(r.to_str(False))
+
+
+if __name__ == '__main__':
+    main()
